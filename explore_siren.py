ed51458a7bf1b0cdfc26d83e035301475636ceb8
Erik Sjoberg <sjobeek@gmail.com>
2020-07-02 22:15:29 +0900

    spatial wavespeed inference working

--- explore_siren.py
+++ explore_siren.py
@@ -93,11 +93,8 @@ class SineLayer(nn.Module):
     
 class Siren(nn.Module):
     def __init__(self, in_features, hidden_features, hidden_layers, out_features, outermost_linear=False, 
-                 first_omega_0=30, hidden_omega_0=30., squared_slowness=3.):
+                 first_omega_0=30, hidden_omega_0=30.):
         super().__init__()
-        
-        self.squared_slowness = torch.nn.Parameter(torch.tensor(float(squared_slowness)))
-        self.squared_slowness.requires_grad=True
 
         self.net = []
         self.net.append(SineLayer(in_features, hidden_features, 
--- surfbreak/waveform_models.py
+++ surfbreak/waveform_models.py
@@ -1,6 +1,7 @@
 import os
 import pkg_resources
 import shutil
+import itertools
 
 import matplotlib.pyplot as plt
 
@@ -24,24 +25,32 @@ from surfbreak.loss_functions import wave_pml_2
         
 class LitSirenNet(pl.LightningModule):    # With no gradient or wave loss, oemega values of (1, 5) work well (best at epochs 3~5)  
     def __init__(self, hidden_features=256, hidden_layers=3, first_omega_0=1.0, hidden_omega_0=5.0, squared_slowness=3.0,
-                 steps_per_vid_chunk=150, learning_rate=1e-4, grad_loss_scale=1e-4, wave_loss_scale=1e-7):
+                 steps_per_vid_chunk=150, learning_rate=1e-4, grad_loss_scale=1e-4, wavefunc_loss_scale=1e-7, 
+                 wavespeed_loss_scale=1e-2):
         """steps_per_vid_chunk defines the single-tensor resampled dataset length"""
         super().__init__()
-        self.save_hyperparameters('first_omega_0', 'hidden_omega_0', 'squared_slowness', 'wave_loss_scale', 'grad_loss_scale',
+        self.save_hyperparameters('first_omega_0', 'hidden_omega_0', 'squared_slowness', 'wavefunc_loss_scale', 'grad_loss_scale','wavespeed_loss_scale',
                                   'hidden_features', 'hidden_layers', 'steps_per_vid_chunk', 'learning_rate' )
         self.model = siren.Siren(in_features=3, 
                                  out_features=1, 
                                  hidden_features=hidden_features,
                                  hidden_layers=hidden_layers, outermost_linear=True,
                                  first_omega_0=first_omega_0,
-                                 hidden_omega_0=hidden_omega_0,
-                                 squared_slowness=squared_slowness) 
+                                 hidden_omega_0=hidden_omega_0)
+        if wavespeed_loss_scale not in [None, 0, 0.]:
+            self.slowness_model = siren.Siren(in_features=2,     
+                                            out_features=1, 
+                                            hidden_features=64,
+                                            hidden_layers=2, outermost_linear=True,
+                                            first_omega_0=1.5,
+                                            hidden_omega_0=10.)
 
         self.steps_per_vid_chunk=steps_per_vid_chunk
         self.learning_rate=learning_rate
         self.grad_loss_scale=grad_loss_scale
         self.squared_slowness = squared_slowness
-        self.wave_loss_scale=wave_loss_scale
+        self.wavefunc_loss_scale=wavefunc_loss_scale
+        self.wavespeed_loss_scale=wavespeed_loss_scale
         
         self.example_input_array = torch.ones(1,1337,3)
 
@@ -65,15 +74,24 @@ class LitSirenNet(pl.LightningModule):    # With no gradient or wave loss, oemeg
             tensorboard_logs['train/grad_loss'] = grad_loss
             tensorboard_logs['train/laplace_loss'] = laplace_loss
 
-        if self.squared_slowness in [None, 0, 0.] or self.wave_loss_scale in [None, 0, 0.]:
+        if self.squared_slowness in [None, 0, 0.] or self.wavefunc_loss_scale in [None, 0, 0.]:
             wavefunc_loss = 0
         else:
-            squared_slowness_tensor = torch.ones_like(coords_out) * self.squared_slowness
+            if self.wavespeed_loss_scale in [None, 0, 0.]:
+                squared_slowness_tensor = torch.ones_like(coords_out) * self.squared_slowness
+                wavespeed_loss = 0
+            else:
+                slow_vals_out, slow_coords_out = self.slowness_model(model_input['masked_coords'][...,1:]) # Omit the first channel (time)
+                squared_slowness_tensor = slow_vals_out.repeat(1,1,3)
+                # Gently push towards known a good value for squared_slowness,                     and heavily penalize all nonsensical negative values
+                wavespeed_loss =  (slow_vals_out - self.squared_slowness).abs().mean()*self.wavespeed_loss_scale - slow_vals_out.clamp(max=0).mean()*10
+                tensorboard_logs['train/wavespeed_loss'] = wavespeed_loss
+                assert squared_slowness_tensor.shape == coords_out.shape
             wave_loss_dict = wave_pml_2(wf_values_out, coords_out, squared_slowness_tensor)
-            wavefunc_loss = wave_loss_dict['diff_constraint_hom']*self.wave_loss_scale # * min(1, (step/ total_steps)**2)
+            wavefunc_loss = wave_loss_dict['diff_constraint_hom']*self.wavefunc_loss_scale # * min(1, (step/ total_steps)**2)
             tensorboard_logs['train/wavefunc_loss'] = wavefunc_loss
 
-        train_loss = mse_loss + avg_loss + grad_loss + laplace_loss + grad_loss + laplace_loss + wavefunc_loss
+        train_loss = mse_loss + avg_loss + grad_loss + laplace_loss + grad_loss + laplace_loss + wavefunc_loss + wavespeed_loss
 
         tensorboard_logs['train/loss'] = train_loss
 
@@ -91,7 +109,7 @@ class LitSirenNet(pl.LightningModule):    # With no gradient or wave loss, oemeg
 
         model_input, ground_truth = self.wf_valid_video_dataset[1]
         cpu_model = self.model.cpu()
-        wf_values_out, coords_out = cpu_model(model_input['all_coords'])
+        wf_values_out, coords_out = cpu_model(model_input['all_coords'])        
 
         wf_gt_txy = ground_truth['all_wavefront_values'].reshape(ground_truth['full_tensor_shape'])
         wf_out_txy = wf_values_out.reshape(ground_truth['full_tensor_shape'])
@@ -108,6 +126,17 @@ class LitSirenNet(pl.LightningModule):    # With no gradient or wave loss, oemeg
 
         self.model.cuda()
 
+        # Calcuate and plot the inferred squared_slowness field in x and y
+        cpu_slow_model =self.slowness_model.cpu()
+        slow_vals_out, slow_coords_out = cpu_slow_model(model_input['all_coords'][...,1:]) # Omit the first channel (time)
+        slow_vals_txy = slow_vals_out.reshape(ground_truth['full_tensor_shape'])
+        fig2 = plt.figure()
+        plt.imshow(slow_vals_txy[0].detach().numpy().T)
+        plt.colorbar()
+        plt.title('Squared slowness estimate')
+        self.logger.experiment.add_figure('squared_slowness', fig2, self.current_epoch)
+        self.slowness_model.cuda()
+
         avg_loss = sum(x["val_loss"] for x in outputs) / len(outputs)
 
         # Pass the accuracy to the `DictLogger` via the `'log'` key.
@@ -115,7 +144,10 @@ class LitSirenNet(pl.LightningModule):    # With no gradient or wave loss, oemeg
         return {"val_loss": avg_loss, "log":tensorboard_logs}
 
     def configure_optimizers(self):
-        return Adam(self.model.parameters(), lr=self.learning_rate)
+        if self.wavespeed_loss_scale not in [None, 0, 0.]:
+            return Adam(itertools.chain(self.model.parameters(), self.slowness_model.parameters()), lr=self.learning_rate)
+        else:
+            return Adam(self.model.parameters(), lr=self.learning_rate)
     
     def setup(self, stage):
         # Train on a dataset consisting of 30-second chunks offset by 30 seconds
